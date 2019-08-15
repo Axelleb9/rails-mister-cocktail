543a45867273a752692929fcab0b4c994786136b
axelle bourneuf <axellebourneuf@hotmail.fr>
2019-08-15 13:03:40 +0200

    MVC cocktails doses

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,40 @@
+class DosesController < ApplicationController
+  before_action :set_dose, only: [:show, :destroy]
+   before_action :set_cocktail, only: [:new, :create]
+
+  def show
+  end
+
+  def new
+  	@dose = Dose.new
+  end
+
+  def create
+  	@dose = Dose.new(doses_params)
+  	@dose.cocktail = @cocktail
+  	if @dose.save
+  		redirect_to cocktail_path(@cocktail)
+  	else
+  		render 'new'
+  	end
+  end
+
+  def destroy
+    @dose.destroy
+    redirect_to dose_path
+  end
+
+  private
+
+  def doses_params
+  	params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+  def set_dose
+  	@dose = Dose.find(params[:id])
+  end
+
+  def set_cocktail
+  	@cocktail = Cocktail.find(params[:cocktail_id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Ingredient.create(name: 'lemon')
+Ingredient.create(name: 'ice')
+Ingredient.create(name: 'rhum')
+Ingredient.create(name: 'passion puree')
+Ingredient.create(name: 'sparkling water')
+Ingredient.create(name: 'banana')
+Ingredient.create(name: 'mint leaves')
+Ingredient.create(name: 'sugar')
